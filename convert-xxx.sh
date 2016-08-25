49dfd2e54dd434bda05ca44cb86767df4a2d1035
old789 <old@old>
2016-08-25 18:53:26 +0300

    some improvement

--- convert-xxx.sh
+++ convert-xxx.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-avconv -y -i /dog/old/dump/xyz.mp4 \
+ffmpeg -y -i /dog/old/dump/xyz.mp4 \
  -s 854x480 -vcodec libx264 -acodec libfaac -threads 0 -b:v 1024k -ab 96k -g 50 -r 25 \
- temp.480.mp4
+ temp.mp4
 
-MP4Box -add temp.480.mp4 /dog/old/vid/xyz.mp4
-
-avconv -y -i temp.480.mp4 -ss 00:00:05 -vframes 1 /dog/old/vid/xyz.jpg
-
-rm temp.480.mp4
+if [ -f temp.mp4 ];then
+    MP4Box -add temp.mp4 /dog/old/vid/xyz.mp4
+    ffmpeg -y -i temp.mp4 -ss 00:00:05 -vframes 1 /dog/old/vid/xyz.jpg
+    rm temp.mp4
+fi
