fa0b14661006ef711623194fe1b93133f9edb82a
old789 <old@old>
2016-08-25 19:06:29 +0300

    some unification, MP4BOX no longer needed

--- convert-xxx.sh
+++ convert-xxx.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-ffmpeg -y -i /dog/old/dump/xyz.mp4 \
- -s 854x480 -vcodec libx264 -acodec libfaac -threads 0 -b:v 1024k -ab 96k -g 50 -r 25 \
+infile='/dog/old/dump/xyz.mp4'
+outfname="/dog/old/vid/xyz.mp4"
+res="854x480"
+bratvid="1024k"
+brataud="96k"
+framerate=25
+
+ffmpeg -y -i "${infile}" \
+ -s ${res} -c:v libx264 -preset slower -b:v ${bratvid} -c:a libfaac -b:a ${brataud} \
+ -movflags +faststart -threads 0 -g 25 -r ${framerate} \
  temp.mp4
 
 if [ -f temp.mp4 ];then
-    MP4Box -add temp.mp4 /dog/old/vid/xyz.mp4
-    ffmpeg -y -i temp.mp4 -ss 00:00:05 -vframes 1 /dog/old/vid/xyz.jpg
+    cp temp.mp4 ${outfname}.mp4
+    ffmpeg -y -i temp.mp4 -ss 00:00:05 -vframes 1 ${outfname}.jpg
     rm temp.mp4
 fi
