aec88905d9598f8a6ac83fd08a583016f1471ca0
old789 <old@old>
2016-08-26 09:44:21 +0300

    variables for temporary files

--- convert-group-xxx.sh
+++ convert-group-xxx.sh
@@ -8,6 +8,9 @@ framerate=23.98
 indir="/dog/old/dump/xyz/"
 oudir="/dog/old/vid/xyz/"
 
+tempsubs="/home/old/temp.ass"
+tempmp4="/home/old/temp.mp4"
+
 if [ ! -d "${indir}" ]; then
     echo Input dir not ready
     exit
@@ -21,23 +24,27 @@ fi
 
 for FILE in *.mkv;
 do
-    cp "RUS Subs/${FILE%mkv}ass" /home/old/temp.ass
+    flt=' '
+    assfile="RUS Subs/${FILE%mkv}ass"
+    if [ -f "$assfile" ]; then
+	cp "$assfile" $tempsubs
+	flt="-vf ass=$tempsubs"
+    fi
+
     ffmpeg -y -i "$FILE" \
 	-map 0:0 -map 0:1 \
 	-s ${res} -c:v libx264 -preset medium -b:v ${bratvid} -c:a libfaac -b:a ${brataud} \
 	-movflags +faststart -threads 0 -g 12 -r ${framerate} \
-	-vf "ass=/home/old/temp.ass" \
-	/home/old/temp.mp4
+	$flt $tempmp4
 
-    if [ -f /home/old/temp.mp4 ];then
+    if [ -f $tempmp4 ];then
 	oufile="${FILE%\ end\ of\ name.mkv}.mp4"
-	oufile="aobs${oufile#begin\ of\ name\ }"
-	mv /home/old/temp.mp4  ${oudir}${oufile}
+	oufile="xyz${oufile#begin\ of\ name\ }"
+	mv $tempmp4 "${oudir}${oufile}"
     fi
 
-    if [ -f /home/old/temp.ass ];then
-	rm /home/old/temp.ass
+    if [ -f $tempsubs ];then
+	rm $tempsubs
     fi
 
 done
-
